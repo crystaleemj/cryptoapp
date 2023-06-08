9c5f5be8c63496979a3b5d46d1648873d80f1e7e
crystaleemj <ceemj_@hotmail.com>
2023-06-08 12:05:05 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/cryptoapp/CryptoappApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cryptoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/cryptoapp/CryptoappApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cryptoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
